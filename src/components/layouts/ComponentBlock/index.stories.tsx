9015eedbd039814f6c9bb8b38ed541acf57fa4fb
Naoto Morishita <mori.nto@outlook.jp>
2026-09-06 15:29:43 +0900

    feat: ComponentBlock

---
+++ src/components/layouts/ComponentBlock/index.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/tanstack-react";
+import { ComponentBlock } from "./index";
+import { ToggleButton } from "../../ui/ToggleButton";
+
+const meta = {
+  title: "Layouts/ComponentBlock",
+  component: ComponentBlock,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  decorators: [
+    (Story) => (
+      <div className="w-80 h-80">
+        <ComponentBlock>
+          <Story />
+        </ComponentBlock>
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof ComponentBlock>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: <ToggleButton />,
+  },
+};
